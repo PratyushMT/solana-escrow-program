b78f16f335b98912210259ff0def543415840a39
Pratyush <tech.pratyush.m@gmail.com>
2025-06-25 16:56:20 +0530

    implemented refund_offer

--- programs/escrow/src/handlers/refund_offer.rs
+++ programs/escrow/src/handlers/refund_offer.rs
@@ -1,14 +1,77 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+use super::shared::{close_token_account, transfer_tokens};
+use crate::{error::ErrorCode, state::Offer};
 
 #[derive(Accounts)]
-pub struct RefundOffer {
-    // RefundOffer (in capitals) is a struct of names accounts that the
-    // refund_offer() function will use.
+pub struct RefundOffer<'info> {
+    pub token_program: Interface<'info, TokenInterface>, //work with either the classic token program or the newer token extensions program
+
+    //used to create accounts
+    pub system_program: Program<'info, System>,
+
+    #[account()]
+    pub maker: Signer<'info>,
+
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        close = maker,
+        has_one = maker,
+        seeds = [b"offer", offer.id.to_le_bytes().as_ref()],
+        bump = offer.bump,
+    )]
+    offer: Account<'info, Offer>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = offer,
+        associated_token::token_program = token_program
+    )]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
 }
 
 // Handle the refund offer instruction by:
 // 1. Returning the tokens from the vault to the maker's account
 // 2. Closing the vault and returning the rent to the maker
-pub fn refund_offer(_context: Context<RefundOffer>) -> Result<()> {
+pub fn refund_offer(context: Context<RefundOffer>) -> Result<()> {
+    //offer account owns the vault
+    let offer_account_seeds = &[
+        b"offer",
+        &context.accounts.offer.id.to_le_bytes()[..],
+        &[context.accounts.offer.bump],
+    ];
+    let signers_seeds = Some(&offer_account_seeds[..]);
+
+    //return the tokens from the vault to the maker's account
+    transfer_tokens(
+        &context.accounts.vault, 
+        &context.accounts.maker_token_account_a, 
+        &context.accounts.vault.amount, 
+        &context.accounts.token_mint_a, 
+        &context.accounts.offer.to_account_info(), 
+        &context.accounts.token_program, 
+        signers_seeds
+    ).map_err(|_| ErrorCode::FailedRefundTransfer)?;
+
+    //close the vault and return the rent to the maker
+    close_token_account(
+        &context.accounts.vault,
+        &context.accounts.maker.to_account_info(),
+        &context.accounts.offer.to_account_info(),
+        &context.accounts.token_program,
+        signers_seeds,
+    ).map_err(|_| ErrorCode::FailedRefundClosure)?;
+
     Ok(())
 }
