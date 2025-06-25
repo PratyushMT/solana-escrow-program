0d3b7029512e5df0b72b36bed43338caba6cfc7e
Pratyush <tech.pratyush.m@gmail.com>
2025-06-25 15:01:05 +0530

    implemented make_offer

--- programs/escrow/src/state/offer.rs
+++ programs/escrow/src/state/offer.rs
@@ -1,6 +1,12 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct Offer {
-    // Details of the offer made, e.g. what who made it and what they want in return.
+    pub id: u64, //offer id
+    pub maker: Pubkey, //maker of the offer
+    pub token_mint_a: Pubkey, //token mint of the token being offered
+    pub token_mint_b: Pubkey, //token mint of the token wanted
+    pub token_b_wanted_amount: u64, //amount of token b being wanted
+    pub bump: u8, //bump for PDA
 }
